93859e6a3b3f0556c6dac8acbe9b410c4933333a
lilxdata <madeline_pignetti@hmc.edu>
2018-10-28 20:42:57 -0700

    foo bar and total done

--- csv-norm.py
+++ csv-norm.py
@@ -66,13 +66,35 @@ for name in names:
   output_names.append(name.upper())
 output_data["FullName"] = output_names
 
-# output_foo = []
-# output_bar = []
-# output_total = []
-# totals = sample_data["TotalDuration"]
-# for total in totals:
-#   new_total = 
+output_foo = []
+output_bar = []
+output_total = []
 
+foos = sample_data["FooDuration"]
+bars = sample_data["BarDuration"]
+
+#fn to convert foo and bar to floats
+def to_float(time):
+    hours, mins, secs = time.split(':')
+    total_secs = float(hours) * 3600 + float(mins) * 60 + float(secs)
+    return total_secs
+
+for foo in foos: 
+  float_foo = to_float(foo)
+  output_foo.append(float_foo)
+  output_total.append(float_foo)
+
+counter = 0
+
+for bar in bars: 
+  float_bar = to_float(bar)
+  output_bar.append(float_bar)
+  output_total[counter] = output_total[counter] + float_bar
+  counter += 1
+
+output_data["FooDuration"] = output_foo
+output_data["BarDuration"] = output_bar
+output_data["TotalDuration"] = output_total
 #need to replace broken character w default unicode
 output_data["Notes"] = sample_data["Notes"]
 
@@ -80,6 +102,6 @@ output_data["Notes"] = sample_data["Notes"]
 output_dataframe=pandas.DataFrame.from_dict(output_data)
 #print("output df", output_dataframe)
 #export to file
-output_dataframe.to_csv('notes123.csv', encoding='utf-8')
+output_dataframe.to_csv('foobar.csv', encoding='utf-8')
 
 
